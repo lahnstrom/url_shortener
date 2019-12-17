1a5acd37b6f45cb5c86e37ecf7a90157cc3a7882
Love Ahnström <love.ahnstrom@gmail.com>
2019-12-17 21:46:11 +0100

    Set default value for short_url

--- url_shortener/links/models.py
+++ url_shortener/links/models.py
@@ -1,8 +1,19 @@
-from django.contrib.auth.models import AbstractUser
+import random
+import string
+
 from django.db.models import CharField, Model
-from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
 
+url_default_length = 8
+
+def generate_short_url():
+    random_string = ''.join(random.choices(
+        string.ascii_uppercase +
+        string.ascii_lowercase +
+        string.digits,
+        k=url_default_length))
+
+    return random_string
 
 class Link(Model):
 
@@ -10,4 +21,5 @@ class Link(Model):
     # https://stackoverflow.com/questions/417142/what-is-the-maximum-length-of-a-url-in-different-browsers
     full_url = CharField(_("Full url"), max_length=2000)
 
-    short_url = CharField(_("Shortened url"), max_length=10, unique=True)
+    short_url = CharField(_("Shortened url"), max_length=url_default_length, unique=True, default=generate_short_url)
+
