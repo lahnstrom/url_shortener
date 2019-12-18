409051b9b53acddc8a7f35838ed326e0756b39d4
Love Ahnström <love.ahnstrom@gmail.com>
2019-12-18 21:38:50 +0100

    Implement validation for url fields

--- url_shortener/links/models.py
+++ url_shortener/links/models.py
@@ -2,7 +2,7 @@ import random
 import string
 import uuid
 
-from django.urls import reverse
+from django.core.validators import RegexValidator
 from django.utils import timezone
 from django.db.models import CharField, Model, IntegerField, UUIDField, DateTimeField
 from django.utils.translation import ugettext_lazy as _
@@ -22,9 +22,16 @@ class Link(Model):
 
     # De facto url max-length is 2000:
     # https://stackoverflow.com/questions/417142/what-is-the-maximum-length-of-a-url-in-different-browsers
-    full_url = CharField(_("Full url"), max_length=2000)
+    full_url = CharField(_("Full url"), max_length=2000, validators=[RegexValidator(
+        regex=r'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)', 
+        message='Enter a valid url starting with http' )])
 
-    short_url = CharField(_("Shortened url"), max_length=url_default_length, unique=True, default=generate_short_url)
+    short_url = CharField(_("Shortened url"), 
+                          max_length=url_default_length,
+                          unique=True,
+                          default=generate_short_url,
+                          validators=[RegexValidator(regex=r'^[a-zA-Z\d_-]{4,8}$', 
+                                                     message=(_("Enter 4-8 alphanumeric characters")))])
 
     # Should increment on every redirect
     redirect_count = IntegerField(_("Redirect Count"), editable=False, default=0)
